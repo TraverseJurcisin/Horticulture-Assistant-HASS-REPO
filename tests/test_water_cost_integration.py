e0d97a08dc4b4fa1d7de871142edfd9fff8719d3
TJ <46633771+TraverseJurcisin@users.noreply.github.com>
2025-07-27 13:57:20 -0400

    Add area water cost helper

--- tests/test_water_cost_integration.py
+++ tests/test_water_cost_integration.py
@@ -6,6 +6,12 @@ def test_estimate_stage_water_cost():
     assert round(cost, 4) == round(0.002 * 6300 / 1000, 4)
 
 
+def test_estimate_area_water_cost():
+    cost = water_usage.estimate_area_water_cost("lettuce", "vegetative", 1.0)
+    expected_ml = water_usage.estimate_area_use("lettuce", "vegetative", 1.0)
+    assert round(cost, 4) == round(0.002 * expected_ml / 1000, 4)
+
+
 def test_estimate_cycle_water_cost():
     cost = water_usage.estimate_cycle_water_cost("lettuce", "california")
     expected_ml = water_usage.estimate_cycle_total_use("lettuce")
