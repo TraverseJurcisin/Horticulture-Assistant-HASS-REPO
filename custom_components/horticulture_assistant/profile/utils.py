f8b0aa9d2b090a10d2921cc35e614b743c439b97
TJ <46633771+TraverseJurcisin@users.noreply.github.com>
2025-10-28 13:08:22 -0400

    Inline fallback display selection for Ruff

--- custom_components/horticulture_assistant/profile/utils.py
+++ custom_components/horticulture_assistant/profile/utils.py
@@ -36,10 +36,11 @@ def ensure_sections(
     candidate_id = plant_id or data.get("plant_id") or data.get("profile_id") or data.get("name")
     fallback_id = _ensure_string(candidate_id, "profile")
     data["plant_id"] = _ensure_string(data.get("plant_id"), fallback_id)
-    if display_name is None:
-        fallback_display = fallback_id
-    else:
-        fallback_display = _ensure_string(display_name, fallback_id)
+    fallback_display = (
+        fallback_id
+        if display_name is None
+        else _ensure_string(display_name, fallback_id)
+    )
     display_candidate = data.get("display_name") or data.get("name") or fallback_display
     data["display_name"] = _ensure_string(display_candidate, fallback_display)
 
@@ -86,10 +87,11 @@ def normalise_profile_payload(
         fallback_id = _ensure_string(data.get("profile_id") or data.get("plant_id"), "profile")
     data["profile_id"] = _ensure_string(data.get("profile_id"), fallback_id)
     data["plant_id"] = _ensure_string(data.get("plant_id"), fallback_id)
-    if display_name is None:
-        fallback_display = fallback_id
-    else:
-        fallback_display = _ensure_string(display_name, fallback_id)
+    fallback_display = (
+        fallback_id
+        if display_name is None
+        else _ensure_string(display_name, fallback_id)
+    )
     display_candidate = data.get("display_name") or data.get("name") or fallback_display
     data["display_name"] = _ensure_string(display_candidate, fallback_display)
 
